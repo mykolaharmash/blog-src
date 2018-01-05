64b824db200a948cf66852f5d2cfaa5f1f9527ee
Nikolay Garmash <garmash.nikolay@gmail.com>
2018-01-05 16:43:20 +0100

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -17,8 +17,10 @@ BUILD_HASH=$(git rev-parse HEAD)
 rm -rf ${TMP_BLOG_DIR}
 
 git clone ${GITHUB_PAGES_REPO} ${TMP_BLOG_DIR}
+
 # Delete everything besides .git
-rm -rf *
+# within blog directory
+rm -rf ${TMP_BLOG_DIR}/*
 
 node generate.js ${TMP_BLOG_DIR}
 node generate-rss.js ${TMP_BLOG_DIR}
